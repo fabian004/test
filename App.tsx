7ecff456f4e08253bbf015014693c843b129357e
Fabian Lomeli <fabianlomeliruiz@gmail.com>
2023-03-30 19:45:02 -0600

    segundo

--- App.tsx
+++ App.tsx
@@ -4,13 +4,14 @@ import {NavigationContainer} from '@react-navigation/native'
 import {ApolloProvider} from '@apollo/client'
 import client from './config/apollo'
 import './config/firebase';
+import { Prueba } from './screens/profile/prueba';
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
     <NavigationContainer>
       <View>
-        <Text>primer test</Text>
+        <Prueba/>
       </View>
     </NavigationContainer>
     </ApolloProvider>
---
+++ screens/profile/prueba.tsx
@@ -0,0 +1,15 @@
+import React, { useState,useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Prueba(){
+    
+    return (
+
+      <View>
+        <Text>segundo test</Text>
+      </View>
+        
+    );
+
+    
+  }
